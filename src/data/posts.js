d5b8e2249746cd028abf8ec61aad578203cf3b97
abhi <abigambhir@gmail.com>
2018-12-01 01:19:56 +0530

    initial structure of the app sette up

---
+++ src/data/posts.js
@@ -0,0 +1,31 @@
+// Already available posts by default
+
+export default[
+    {
+        userName: 'abhigambhir.js',                                 //By the way thats my original instagram id too in case you want to follow me
+        userImage: '../assets/images/abhigambhir.png',
+        postImage: '../assets/images/abhipost.png',
+        likes: 55,
+        hasBeenLiked: false,
+        caption: 'New setup..',
+        filter: 'normal'
+    },
+    {
+        userName: 'tanushadhawan',
+        userImage: '../assets/images/tanushadhwan.png',
+        postImage: '../assets/images/tanushapost.png',
+        likes: 97,
+        hasBeenLiked: true,
+        caption: 'Doodle art',
+        filter: 'normal'
+    },
+    {
+        userName: 'stickhubindia',
+        userImage: '../assets/images/stickhubindia.png',
+        postImage: '../assets/images/devstickers.png',
+        likes: 67,
+        hasBeenLiked: false,
+        caption: 'Dev Stickers',
+        filter: 'nashville'
+    }
+];
